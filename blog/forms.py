9f624530d9e2858e186b2c0e145e26e41c1943e4
unFamiliaftGuy <unflguy@gmail.com>
2019-06-12 14:57:15 +1000

    Some minor changes

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class FilterForm(forms.Form):
-	author = forms.CharField(label='Author name', max_length=100, required=False)
+	author = forms.CharField(label='Author name:', max_length=100, required=False)
