ddcadf4e665710c8531b3731b55487e2e2a52b27
unFamiliaftGuy <unflguy@gmail.com>
2019-06-13 20:32:20 +1000

    Add favicon Redesign filter form

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,14 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+
 
 class FilterForm(forms.Form):
-	author = forms.CharField(label='Author name:', max_length=100, required=False)
+    author = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Author name'}), label='', max_length=100,
+                             required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(FilterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-filterForm'
+        self.helper.form_class = 'form-inline'
