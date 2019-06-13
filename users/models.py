3587cd211d1f2379e88872cf6924e85267976ffe
JIeHb <gribak98@list.ru>
2019-06-14 08:07:28 +1000

    View fixes

--- users/models.py
+++ users/models.py
@@ -4,17 +4,17 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
-    position = models.CharField(
-        choices=[('Студент', 'Студент'),
-                 ('Преподаватель', 'Преподаватель'),
-                 ('Ученый', 'Ученый'),
-                 ('Другая', 'Другая')
-                 ],
-        default='Другая',
-        max_length=20
-    )
+	user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+	image = models.ImageField(default='default.png', upload_to='profile_pics')
+	position = models.CharField(
+		choices=[('Студент', 'Студент'),
+				('Преподаватель', 'Преподаватель'),
+				('Ученый', 'Ученый'),
+				('Другая', 'Другая')
+				],
+		default='Другая',
+		max_length=20
+	)
 
 	access = models.CharField(
 		choices = [('Пользователь', 'Пользователь'),
@@ -25,13 +25,13 @@ class Profile(models.Model):
 		max_length = 20
 	)
 
-    def __str__(self):
-        return f'{self.user.username} profile'
+	def __str__(self):
+		return f'{self.user.username} profile'
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.height > 150 or img.width > 150:
-            output_size = (150, 150)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+	def save(self, *args, **kwargs):
+		super(Profile, self).save(*args, **kwargs)
+		img = Image.open(self.image.path)
+		if img.height > 150 or img.width > 150:
+			output_size = (150, 150)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
