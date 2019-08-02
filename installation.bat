9238d71925b7adef940df4b060b5bb0f7e3e47e9
JIeHb <gribak98@list.ru>
2019-08-02 10:38:45 +1000

    Updated installation file

--- installation.bat
+++ installation.bat
@@ -3,3 +3,7 @@ pip install django-crispy-forms --user
 pip install django-tinymce --user
 pip install django-pillow --user
 pip install django-autocomplete-light --user
+cd %cd%
+py manage.py makemigrations
+py manage.py migrate
+py manage.py runserver
